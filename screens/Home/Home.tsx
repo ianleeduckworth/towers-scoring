a58d46f880b951a6e20e97d1225c942b860b58a8
Ian Duckworth <ianleeduckworth@gmail.com>
2021-01-18 23:42:10 -0500

    updated home screen

--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { RootStackParamList } from '../RootStack';
 import { Button } from '../../components/Button/Button';
 import { globalStyles } from '../../styles/global';
@@ -16,7 +16,28 @@ type HomeScreenProps = {
 
 export const HomeScreen = ({ navigation }: HomeScreenProps) => (
   <View style={globalStyles.container}>
-    <Text>Home Screen</Text>
-    <Button text="To Rules Screen" onPress={() => navigation.navigate('Rules')} />
+    <View style={styles.textContainer}>
+      <Text style={styles.italics}>
+        No current games.  Please click NEW GAME to begin a new game
+      </Text>
+    </View>
+    <View style={styles.button}>
+      <Button text="New Game" onPress={() => navigation.navigate('Rules')} />
+    </View>
   </View>
 );
+
+const styles = StyleSheet.create({
+  button: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  italics: {
+    ...globalStyles.paragraphText,
+    fontStyle: 'italic',
+  },
+  textContainer: {
+    flex: 10,
+    justifyContent: 'center',
+  },
+});
